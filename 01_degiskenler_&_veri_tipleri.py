8b46bed2122c5d14a9b16c07bb6db71b39217a9b
Efraxon <gunluoglugulay@gmail.com>
2023-03-04 13:01:41 +0300

    01-Python-101

---
+++ 01_degiskenler_&_veri_tipleri.py
@@ -0,0 +1,37 @@
+# değişken tanımlama dersi
+
+x = 5
+
+y = "Hello world"
+
+z = True
+
+# VERİ TÜRLERİ
+
+# 1. Tam sayilar => Integer
+
+x = 15
+
+y = -15
+
+z = 0
+
+# 2. ONDALIK SAYILAR -- FLOAT
+
+x = 3.43654
+
+a = 1.0
+
+y = -8.949
+
+z = 0.1
+
+# 3. METİN TÜRLERİ STRİNG
+
+selam = "HOJGELDİNİZ"
+
+ad = "YAKUBİ"
+
+mesaj = selam + " " + ad
+
+print(mesaj)
