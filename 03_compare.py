508aa4d10ed3434b9e59483e21a945e6a7a0aa96
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-03-11 13:01:35 +0300

    compare is done

---
+++ 03_compare.py
@@ -0,0 +1,18 @@
+# Equal?
+x = 5
+y = 10
+
+print(x == y)
+print(x == 5)
+
+# !Equal?
+
+print(x != y)
+print(y != 10)
+
+# Small?
+
+print("x < y:", x < y)
+print("y < x:", y < x)
+print("x + 5 < y:", x + 5 < y)
+
