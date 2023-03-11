0d0c4795e4ce7cd8c5ffe85234bc2249b5c0d484
Yakup <ykpcr.yakupacar@gmail.com>
2023-03-11 12:44:02 +0300

    arithmatic operators are over

---
+++ 02_arithmatic_operators.py
@@ -0,0 +1,59 @@
+"""
+    addition => +
+    extraction  => -
+    multiplication => *
+    division => /
+    full division => //
+    mode => %
+    exponentiation => **
+"""
+
+# Addiction
+a = 5
+b = 4
+c = a + b
+
+print(c)
+
+# Extraction
+d = 10
+e = 6
+f = d - e
+print(f)
+
+# Multiplication
+g = 6
+h = 3
+i = g * h
+print(i)
+
+# Division
+j = 121
+k = 7
+l = j / k
+print(l)
+
+# Full Division
+m = j // k
+
+print(m)
+
+n = -86
+o = n // k
+
+print(o)
+
+# Mode
+p = j % k
+print(p)
+r = n % k
+print(r)
+
+# Exponentiation
+s = 2
+t = 3
+u = s ** t
+print(u)
+
+# Process Priority
+print(5 + 4 ** 3)
