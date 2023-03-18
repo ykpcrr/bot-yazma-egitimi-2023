f86909122602f3f4875e9187a16b7323c3c6f074
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-03-18 12:54:37 +0300

    membership_operators has done

---
+++ 06_Membership_operators.py
@@ -0,0 +1,14 @@
+# Membership Operators: in, not in
+
+list = [1, 2, 3, 4, 5]
+
+# in
+
+print(4 in list) # true
+print(6 in list) # false
+
+# not in
+
+print(4 not in list)
+print(6 not in list)
+
