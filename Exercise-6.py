eccff2bebde65cd47837d9d06384d19bdae0d782
Yakup Acar <114645336+ykpcrr@users.noreply.github.com>
2023-03-18 19:56:54 +0300

    Delete Exercise-6.py

--- Exercise-6.py
+++
@@ -1,7 +0,0 @@
-students = ["yakup", "taha", "hamza"]
-print("hamza" in students)
-print("furkan" in students)
-
-fruits = ["apple", "pear"]
-print("apple" not in fruits)
-print("cherry" not in fruits)
