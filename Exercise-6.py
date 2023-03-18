dc8d719f04bb02109ee2fc0e9a417525f76e304c
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-03-18 09:46:53 +0300

    all homeworks done

---
+++ Exercise-6.py
@@ -0,0 +1,7 @@
+students = ["yakup", "taha", "hamza"]
+print("hamza" in students)
+print("furkan" in students)
+
+fruits = ["apple", "pear"]
+print("apple" not in fruits)
+print("cherry" not in fruits)
