1fead3ff173aefd99cc1cfa9310d1b47bd219b03
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-03-23 20:48:28 +0300

    Update exersice-7

--- Exercise-7.py
+++ Exercise-7.py
@@ -9,12 +9,12 @@ index = weight / height ** 2
 print(f"Dear {name}, your weight index is: {index}")
 
 if index < 18.4:
-    print(f"I an sorry but i have to tell the truth you are weak")
+    print("you are weak")
 elif index > 18.4 and index < 24.99:
-    print(f"you are good i am waiting to be great")
+    print("you are good ")
 elif index > 24.99 and index < 29.9:
-    print(f"You are fat boy")
+    print("You are fat")
 elif index > 29.9 and index < 45.9:
-    print(f"you are a obese")
+    print("you are an obese")
 else:
     print("your datas are not true")
