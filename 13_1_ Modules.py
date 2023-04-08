8846f8441fa72e13e72cbdddbca0df5ea0341679
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-04-08 13:10:44 +0300

    Modules 1-5 have done

---
+++ 13_1_ Modules.py
@@ -0,0 +1,7 @@
+# import of modules
+
+import math
+
+print(math.sqrt(16))
+print(math.pow(2, 10))
+print(math.pi)
---
+++ 13_4_Standard_modules.py
@@ -0,0 +1,13 @@
+# random
+
+import random
+import time
+import calendar
+
+print(random.randint(1, 100))
+# time.sleep(1)
+print(random.random())
+print(time.timezone)
+
+print(calendar.month(2023, 4))
+
