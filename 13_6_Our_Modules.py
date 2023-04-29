f795ca6ebd9cb18ecd8d3727e92b1510d1c392c1
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-04-29 11:06:45 +0300

    Edit done

--- 13_6_Our_Modules.py
+++ 13_6_Our_Modules.py
@@ -5,7 +5,7 @@
 # print(Math_Calculations.collect(1, 3))
 # print(Math_Calculations.interest(1, 3))
 # print(Math_Calculations.multiply(1, 3))
-# print(Math_Calculations.plenty(1, 3))
+# print(Math_Calculations.division(1, 3))
 
 
 # Second Way
--- Math_Calculations.py
+++ Math_Calculations.py
@@ -10,5 +10,5 @@ def multiply(a, b):
     return a * b
 
 
-def plenty(a, b):
+def division(a, b):
     return a / b
