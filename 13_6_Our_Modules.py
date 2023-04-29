1ab631a3abfef5a115d768c687878aa874a9c42c
ykpcrr <ykpcr.yakupacar@gmail.com>
2023-04-29 10:33:19 +0300

    Math modules has created

---
+++ 13_6_Our_Modules.py
@@ -0,0 +1,16 @@
+# Import First Way
+
+# Import Math_Calculations
+
+# print(Math_Calculations.collect(1, 3))
+# print(Math_Calculations.interest(1, 3))
+# print(Math_Calculations.multiply(1, 3))
+# print(Math_Calculations.plenty(1, 3))
+
+
+# Second Way
+
+from Math_Calculations import multiply, interest
+
+print(multiply(2, 6))
+print(interest(1,101000))
---
+++ Math_Calculations.py
@@ -0,0 +1,14 @@
+def collect(a, b):
+    return a + b
+
+
+def interest(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def plenty(a, b):
+    return a / b
